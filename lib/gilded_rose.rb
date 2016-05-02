28b012a10a337c664e341dffcf66e3855788d0cf
BEL <jenoid2@gmail.com>
2016-05-02 18:35:21 +0800

    Gilded Rose refactor 2

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,6 +10,10 @@ class GildedRose
     @items.each do |item|
       next if item.name == 'Sulfuras, Hand of Ragnaros'
       item.sell_in = item.sell_in - 1
+      if item.quality > 0
+        n = item.sell_in <= 0 ? 2 : 1
+        item.quality = item.quality - n  
+      end
     end
 
   end
